48ebd179a2a35186c9ab2f756f93d6d16d5e77b4
ltc <daqiuqu@live.cn>
2014-10-15 09:47:06 +0800

    project init

---
+++ src/com/example/wifi_signal/MyActivity.java
@@ -0,0 +1,80 @@
+/*
+package com.example.wifi_signal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+*/
+
+
+//package com.example.wifistrength;
+package com.example.wifi_signal;
+
+import java.util.List;
+
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyActivity extends Activity {
+	
+	TextView tv;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        String wserviceName = Context.WIFI_SERVICE;
+    	WifiManager wm = (WifiManager) getSystemService(wserviceName); 
+    	
+        WifiInfo info = wm.getConnectionInfo();   
+        int strength = info.getRssi();
+        int speed = info.getLinkSpeed();  
+        String units = WifiInfo.LINK_SPEED_UNITS;  
+        String ssid = info.getSSID();  
+	String mac_connect = info.getMacAddress();
+        
+        tv  = (TextView) findViewById(R.id.textView1);
+	tv.setMovementMethod(ScrollingMovementMethod.getInstance());
+
+        List<ScanResult> results = wm.getScanResults();
+        String otherwifi = "The existing network is: \n\n";
+        
+        for (ScanResult result : results) {  
+            otherwifi += result.SSID  + ":" + result.level + " mac:" + result.BSSID + "\n";
+        }
+        
+        String text = "We are connecting to " + ssid + " " + mac_connect + " at " + String.valueOf(speed) + "   " + String.valueOf(units) + ". Strength : " + strength;
+        otherwifi += "\n\n";
+        otherwifi += text;
+        System.out.println(otherwifi);
+        
+        tv.setText(otherwifi);
+        
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        //setContentView(R.layout.main);
+        return true;
+    }
+    
+}
