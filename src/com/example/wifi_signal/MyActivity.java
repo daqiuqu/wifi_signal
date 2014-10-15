734d818d7c0711b721622932e0d3c598b2bc1cbc
ltc <daqiuqu@live.cn>
2014-10-15 15:20:06 +0800

    get wifi signal info every sec

--- src/com/example/wifi_signal/MyActivity.java
+++ src/com/example/wifi_signal/MyActivity.java
@@ -23,50 +23,69 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
 import android.content.Context;
 import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.widget.TextView;
-import android.widget.Toast;
+//import android.widget.Toast;
 
 public class MyActivity extends Activity {
 	
 	TextView tv;
+	private Handler myHandler;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        String wserviceName = Context.WIFI_SERVICE;
-    	WifiManager wm = (WifiManager) getSystemService(wserviceName); 
-    	
-        WifiInfo info = wm.getConnectionInfo();   
-        int strength = info.getRssi();
-        int speed = info.getLinkSpeed();  
-        String units = WifiInfo.LINK_SPEED_UNITS;  
-        String ssid = info.getSSID();  
-	String mac_connect = info.getMacAddress();
-        
-        tv  = (TextView) findViewById(R.id.textView1);
-	tv.setMovementMethod(ScrollingMovementMethod.getInstance());
-
-        List<ScanResult> results = wm.getScanResults();
-        String otherwifi = "The existing network is: \n\n";
-        
-        for (ScanResult result : results) {  
-            otherwifi += result.SSID  + ":" + result.level + " mac:" + result.BSSID + "\n";
-        }
-        
-        String text = "We are connecting to " + ssid + " " + mac_connect + " at " + String.valueOf(speed) + "   " + String.valueOf(units) + ". Strength : " + strength;
-        otherwifi += "\n\n";
-        otherwifi += text;
-        System.out.println(otherwifi);
-        
-        tv.setText(otherwifi);
-        
+       	myHandler = new Handler();
+        myHandler.post(wifiTimer);
     }
+
+	protected void onDestroy(Bundle savedInstanceState) {
+		myHandler.removeCallbacks(wifiTimer);
+	}
+
+	private Runnable wifiTimer = new Runnable() {
+		public void run() {
+			getWifiInfo();
+			myHandler.postDelayed(this, 1000);
+		}
+	};
+
+	public void getWifiInfo() {
+		String wserviceName = Context.WIFI_SERVICE;
+		WifiManager wm = (WifiManager) getSystemService(wserviceName); 
+		if (!wm.isWifiEnabled())
+			wm.setWifiEnabled(true);
+		wm.startScan();
+		
+		WifiInfo info = wm.getConnectionInfo();   
+		int strength = info.getRssi();
+		int speed = info.getLinkSpeed();  
+		String units = WifiInfo.LINK_SPEED_UNITS;  
+		String ssid = info.getSSID();  
+		String mac_connect = info.getMacAddress();
+		
+		tv  = (TextView) findViewById(R.id.textView1);
+		tv.setMovementMethod(ScrollingMovementMethod.getInstance());
+		
+		List<ScanResult> results = wm.getScanResults();
+		String otherwifi = "The existing network is: \n\n";
+		
+		for (ScanResult result : results) {  
+		    otherwifi += result.SSID  + ":" + result.level + " mac:" + result.BSSID + "\n";
+		}
+		
+		String text = "We are connecting to " + ssid + " " + mac_connect + " at " + String.valueOf(speed) + "   " + String.valueOf(units) + ". Strength : " + strength;
+		otherwifi += "\n\n";
+		otherwifi += text;
+		System.out.println(otherwifi);
+		
+		tv.setText(otherwifi);
+	}
 
 
     @Override
